18dd7db0c1c4bb361d8362e389899229021b1bb9
monkey <monkey>
2022-11-15 10:08:27 +0800

    feat：拦截open和send成功

--- src/native.js
+++ src/native.js
@@ -1,18 +1,23 @@
-import './tools/toolsv2';
+import './tools/tool';
 import { network } from './tools/network';
 const container = document.getElementById('root');
 
-const body = document.createElement('span');
+const body0 = document.createElement('span');
+body0.className = 'span';
+const body2 = document.createElement('span');
+body2.className = 'span';
 
 function getName() {
   console.log('I am click, I am native js');
   network.get('/api/use');
 }
 
-body.addEventListener('click', getName, false);
-body.addEventListener('click', getName, false);
-body.innerHTML = 'I am native js';
+body0.addEventListener('click', getName, false);
+body0.addEventListener('click', getName, false);
+body0.innerHTML = 'I am native js';
 
 const wrapper = document.createElement('div');
-wrapper.appendChild(body);
+wrapper.className = 'container';
+wrapper.appendChild(body0);
+wrapper.appendChild(body2);
 container.appendChild(wrapper);
--- src/tools/tool.js
+++ src/tools/tool.js
@@ -1,38 +1,56 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 const interceptEvent = ['click', 'dbclick'];
 
 const mm = new Map();
+
+// 拦截XMLHttpRequest
+const nativeOpen = XMLHttpRequest.prototype.open;
+const nativeSend = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.open = function (...args) {
+  const { method, url } = args;
+  console.log('请求方法和地址======>', args);
+  nativeOpen.call(this, ...args);
+};
+XMLHttpRequest.prototype.send = function (body) {
+  console.log('请求body======>', body);
+  nativeSend.apply(this, body);
+};
 
 if (window.addEventListener) {
   const nativeAddEventListener = EventTarget.prototype.addEventListener;
   //   intercept
   EventTarget.prototype.addEventListener = function (event, handler, options) {
-    console.log('add========>', event, handler, options);
-    let fn = handler;
+    console.log('add========>', event, handler, options, this);
+    let fn = null;
     if (typeof handler !== 'function') {
       throw new Error('callback is not function');
     }
+    // 改写handler
     if (interceptEvent.includes(event)) {
       fn = function (...args) {
         // 自定义函数
-        const uuid = uuidv4();
-        let capture = false;
-        if (typeof options === 'boolean') {
-          capture = options;
-        } else if (Object.prototype.toString(options) === '[object Object]') {
-          capture = options.capture;
-        }
-        if (!capture) {
-          sessionStorage.setItem('uuid', uuid);
-          handler.yanpin = fn;
-          console.log(`触发了${event}, uuid=`, uuid, event);
-        }
+        // const uuid = uuidv4();
+        // let capture = false;
+        // if (typeof options === 'boolean') {
+        //   capture = options;
+        // } else if (Object.prototype.toString(options) === '[object Object]') {
+        //   capture = options.capture;
+        // }
+        // if (!capture) {
+        // sessionStorage.setItem('uuid', uuid);
+        // handler.yanpin = fn;
+        // console.log(`触发了${event}, uuid=`, uuid, event);
+        // }
 
         handler.call(this, ...args);
       };
+    } else {
+      fn = handler;
     }
+    // 存储改写的handler
     mm.set(handler, fn);
-    nativeAddEventListener(event, fn, options);
+    // 注册
+    nativeAddEventListener.call(this, event, fn, options);
   };
 
   // 卸载函数
@@ -42,8 +60,9 @@ if (window.addEventListener) {
     handler,
     options
   ) {
-    // const fn = mm.get(handler);
-    // console.log('remove=====>', mm.size);
-    // nativeRemoveEventLisener(event, handler.yanpin, options);
+    const fn = mm.get(handler);
+    mm.delete(handler);
+    console.log('remove=====>', mm.size);
+    nativeRemoveEventLisener(event, fn, options);
   };
 }
